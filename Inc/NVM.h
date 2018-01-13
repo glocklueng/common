62cd12cf4d76ac82ea054e001df00cb66007677e
Tyler D <tylerdibo@hotmail.com>
2018-01-13 02:47:26 -0500

    Small fixes to NVM library

--- Inc/NVM.h
+++ Inc/NVM.h
@@ -5,6 +5,6 @@
 
 int NVM_Init(void);
 int NVM_Store_Variables(uint16_t location, int size, uint16_t* data);
-int NVM_Retrieve_Variables(uint16_t location, int size, uint16_t** data);
+int NVM_Retrieve_Variables(uint16_t location, int size, uint16_t* data);
 
 #endif /* __NVM_H */
--- Src/NVM.c
+++ Src/NVM.c
@@ -3,15 +3,21 @@
 
 //TODO: implement VirtAddVarTab
 
+extern uint16_t VirtAddVarTab[NB_OF_VAR];
+
 int NVM_Init(void){
+	HAL_FLASH_Unlock();
 	return EE_Init(); //Initialize pages
 }
 
 //Write array of variables one by one
 int NVM_Store_Variables(uint16_t location, int size, uint16_t* data){
+	if((size+location) >= NB_OF_VAR){ //Not enough memory locations to store all data
+		return -1;
+	}
 	int status = 0;
 	for(int i = size-1; i >= 0; i--){
-		status = EE_WriteVariable(location+i, data[i]);
+		status = EE_WriteVariable(VirtAddVarTab[location+i], data[i]);
 		if(status != 0){
 			return status;
 		}
@@ -21,11 +27,14 @@ int NVM_Store_Variables(uint16_t location, int size, uint16_t* data){
 }
 
 //Read array of variables one by one
-int NVM_Retrieve_Variables(uint16_t location, int size, uint16_t** data){
+int NVM_Retrieve_Variables(uint16_t location, int size, uint16_t* data){
+	if((size+location) >= NB_OF_VAR){ //Not enough memory locations to retrieve all data
+		return -1;
+	}
 	int readStatus = 0;
 	int status = 0;
 	for(int i = 0; i < size; i++){
-		status = EE_ReadVariable(location+i, data[i]);
+		status = EE_ReadVariable(VirtAddVarTab[location+i], &(data[i]));
 		if(status == NO_VALID_PAGE){
 			return status;
 		}else if(status == 1){
